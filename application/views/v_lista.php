48fdbeaafebbd164d141c83841487be16dcb92aa
muhrizky <muhrizqi@ce.undip.ac.id>
2018-12-02 03:46:07 +0700

    add pengkondisian notif

--- application/views/v_lista.php
+++ application/views/v_lista.php
@@ -32,6 +32,7 @@
   
 
   <body class="nav-md">
+  <?php include 'v_notif.php' ?>
     <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
--- application/views/v_notif.php
+++ application/views/v_notif.php
@@ -28,8 +28,8 @@
      <!-- Custom modal dialog -->
      <link rel="stylesheet" href="<?php echo base_url('assets/custome/style.css'); ?>">
 
-  <!-- modal notifikasi -->
-  <div class="modal fade"  id="modalnotif" role="dialog" >
+   <!-- modal notifikasi -->
+   <div class="modal fade"  id="modalnotif" role="dialog" >
                     <div class="modal-dialog modal-sm">
                       <div class="modal-content">
 
@@ -38,15 +38,25 @@
                           </button>
                           <h4 class="modal-title" id="myModalLabel2"> Peringatan !!</h4>
                         </div>
+                        
+                        <?php if($this->session->userdata('jabatan') == 'Operator'): ?>
                         <div class="modal-body">
-                          <p>Inkubator mengalami Overheat, Segera Turunkan Suhu dengan set point atau dengan mematikan inkubator bayi, Pilih konfirmasi untuk </p>
+                          <p>Inkubator mengalami <b>High Temperature</b>, Segera turunkan Suhu, Pilih konfirmasi untuk melihat riwayat detail sensor ke menu kontrol</p>
                         </div>
                         <div class="modal-footer">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-                          
-                          <a href="<?php echo base_url();?>beranda" type="button" class="btn btn-primary">Konfirmasi</a>
+                         <a href="<?php echo base_url();?>kontrol" type="button" class="btn btn-primary">Konfirmasi</a>
                         </div>
-
+                        
+                        <?php else: ?>
+                        <div class="modal-body">
+                          <p>Inkubator mengalami <b>High Temperature</b>, Segera turunkan Suhu, Pilih konfirmasi untuk melihat riwayat detail sensor ke menu data</p>
+                        </div>
+                        <div class="modal-footer">
+                         <a href="<?php echo base_url();?>data" type="button" class="btn btn-primary">Konfirmasi</a>
+                        </div>
+                        <?php endif; ?>
+                        
+                      
                       </div>
                     </div>
                   </div>
@@ -54,48 +64,11 @@
 
 
 
+
+
     <!-- jQuery -->
     <script src="<?php echo base_url();?>assets/vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url();?>assets/vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="<?php echo base_url();?>assets/vendors/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="<?php echo base_url();?>assets/vendors/Chart.js/dist/Chart.min.js"></script>
-    <!-- easy-pie-chart -->
-    <script src="<?php echo base_url();?>assets/vendors/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js"></script>
-    <!-- jQuery Sparklines -->
-    <script src="<?php echo base_url();?>assets/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- morris.js -->
-    <script src="<?php echo base_url();?>assets/vendors/raphael/raphael.min.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/morris.js/morris.min.js"></script>
-    <!-- gauge.js -->
-    <script src="<?php echo base_url();?>assets/vendors/gauge.js/dist/gauge.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <!-- Skycons -->
-    <script src="<?php echo base_url();?>assets/vendors/skycons/skycons.js"></script>
-    <!-- Flot -->
-    <script src="<?php echo base_url();?>assets/vendors/Flot/jquery.flot.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/Flot/jquery.flot.time.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="<?php echo base_url();?>assets/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="<?php echo base_url();?>assets/vendors/DateJS/build/date.js"></script>
-   <!-- bootstrap-daterangepicker -->
-   <script src="<?php echo base_url();?>assets/vendors/moment/min/moment.min.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-    <!-- bootstrap-datetimepicker -->    
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-    <!-- jQuery Knob -->
-    <script src="<?php echo base_url();?>assets/vendors/jquery-knob/dist/jquery.knob.min.js"></script>
+    
     <!-- Custom Theme Scripts -->
     <script src="<?php echo base_url();?>assets/build/js/custom.js"></script>
 
